61ed1c9084fca1ce5fb9209d057dad2b9a5430a3
byungs2 <byungs2@naver.com>
2020-11-26 17:23:15 +0900

    2020-11-26 17:23

--- admin/src/adminTable.js
+++ admin/src/adminTable.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {PasswordInput ,Create, EditButton, Filter, TextInput, ReferenceInput, SelectInput,List, Datagrid, TextField, SimpleForm,Edit, DeleteButton } from 'react-admin';
+import {PasswordInput ,Create, EditButton, Filter, TextInput, ReferenceInput, SelectInput,List, Datagrid, TextField, SimpleForm,Edit} from 'react-admin';
 // ReferenceField의 source는 forignkey, reference는 참조할 테이블 명시하는 것
 
 
--- admin/src/memberTable.js
+++ admin/src/memberTable.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Create, EditButton, Filter, TextInput, ReferenceInput, SelectInput, ImageField, List, Datagrid, TextField, ImageInput,SimpleForm,Edit, DeleteButton } from 'react-admin';
+import {Create, EditButton, Filter, TextInput, ReferenceInput, SelectInput, ImageField, List, Datagrid, TextField, ImageInput,SimpleForm,Edit} from 'react-admin';
 // ReferenceField의 source는 forignkey, reference는 참조할 테이블 명시하는 것
 
 
