d087b7edf75e1185d7145bdd313dfe9186dc4c01
byungs2 <byungs2@naver.com>
2020-12-18 11:47:33 +0900

    2020-12-18 11:47

--- admin/src/authProvider.js
+++ admin/src/authProvider.js
@@ -3,32 +3,26 @@ import { fetchUtils } from 'ra-core';
 export default {
     // called when the user attempts to log in
     login: ({ username, password }) => {
-        if('master' === username && 'master' === password ){
-            localStorage.setItem('username', username);
-            // accept all username/password combinations
-            return Promise.resolve();
-        }else{
-            const httpClient = fetchUtils.fetchJson;
-            const formData = new FormData();
-            formData.append('username', username);
-            formData.append('password', password);
-            const servicesHost = 'http://localhost:8082';
-            return httpClient(servicesHost + '/login',{
-                method : 'post',
-                body : JSON.stringify({"username" : username})
-            })
-            .then(function(res){
-                const json = res.json;
-                if(json.adminId === username && json.adminPw === password ){
-                    localStorage.setItem('username', username);
-                    // accept all username/password combinations
-                    return Promise.resolve();
-                }else{
-                    return Promise.reject();
-                }
-            })
-        }
-    }, 
+        const httpClient = fetchUtils.fetchJson;
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('password', password);
+        const servicesHost = 'http://localhost:8082';
+        return httpClient(servicesHost + '/login',{
+            method : 'post',
+            body : JSON.stringify({"username" : username})
+        })
+        .then(function(res){
+            const json = res.json;
+            if(json.adminId === username && json.adminPw === password ){
+                localStorage.setItem('username', username);
+                // accept all username/password combinations
+                return Promise.resolve();
+            }else{
+                return Promise.reject();
+            }
+        })    
+    },
     // called when the user clicks on the logout button
     logout: () => {
         localStorage.removeItem('username');
